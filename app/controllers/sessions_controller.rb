27a8fa882d897b96490bbdd186d54379f4283ced
Emily Jirles <emily.jirles@gmail.com>
2018-06-22 19:11:53 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    raise params.inspect
     @user = User.find_by(name: params[:user][:name])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
