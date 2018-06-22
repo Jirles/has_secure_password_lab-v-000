fc2d705739da033735db51b20fa7cd357a362c9f
Emily Jirles <emily.jirles@gmail.com>
2018-06-22 18:54:23 +0000

    automatically backed up by learn

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -8,14 +8,14 @@ describe "Secure Password Lab App" do
       expect(page).to have_content("Login")
     end
 
-    it 'login page has a form for users to login by providing their name, password, and a password confirmation' do 
-      visit root_path 
+    it 'login page has a form for users to login by providing their name, password, and a password confirmation' do
+      visit root_path
 
       expect(page).to have_selector("form")
       expect(page).to have_field(:name)
       expect(page).to have_field(:password)
       expect(page).to have_field(:password_confirmation)
-    end 
+    end
 
     it 'login page/root path also has a link to signup for new users' do
       visit root_path
