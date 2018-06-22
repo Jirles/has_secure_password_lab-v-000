888401c9322835e730c46dfb64891c7c24129156
Emily Jirles <emily.jirles@gmail.com>
2018-06-22 18:39:02 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:name], password: params[:password])
-    if @user.save
+    @user = User.new(user_params)
+    if name_present? && @user.save
       session[:user_id] = @user.id
       redirect_to welcome_path
     else
