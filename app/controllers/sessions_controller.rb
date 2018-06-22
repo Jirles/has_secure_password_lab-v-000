0aa5fe359941f626901ad6a48b54bb35b3f45fe3
Emily Jirles <emily.jirles@gmail.com>
2018-06-22 18:58:28 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,13 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to welcome_path
     else
-      redirect_to login_path
+      redirect_to root_path
     end
   end
 
   def destroy
     session.clear
-    redirect_to login_path
+    redirect_to root_path
   end
 
 end
