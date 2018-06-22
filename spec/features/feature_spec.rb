f1b54819da3643334cf92a01b37b57ef7782f278
Emily Jirles <emily.jirles@gmail.com>
2018-06-22 18:54:00 +0000

    automatically backed up by learn

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,18 +1,27 @@
 require 'rails_helper'
 
-describe "Secure Password Lab App" do 
-  context "user features" do 
-    it 'takes users to the login page as the root path' do 
+describe "Secure Password Lab App" do
+  context "user features" do
+    it 'takes users to the login page as the root path' do
       visit root_path
 
       expect(page).to have_content("Login")
-    end 
+    end
 
-    it 'login page/root path also has a link to signup for new users' do 
+    it 'login page has a form for users to login by providing their name, password, and a password confirmation' do 
       visit root_path 
 
-      expect(page).to have_link("Signup")
+      expect(page).to have_selector("form")
+      expect(page).to have_field(:name)
+      expect(page).to have_field(:password)
+      expect(page).to have_field(:password_confirmation)
     end 
-  end 
-  
-end 
+
+    it 'login page/root path also has a link to signup for new users' do
+      visit root_path
+
+      expect(page).to have_link("Signup")
+    end
+  end
+
+end
