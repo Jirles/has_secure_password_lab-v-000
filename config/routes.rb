7a6de93bd6b28308c3ff3dee2d817ff3730d566d
Emily Jirles <emily.jirles@gmail.com>
2018-06-22 18:54:44 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
-  get '/login' => 'sessions#new'
+  root 'sessions#new'
   post '/logout' => 'sessions#create'
   get '/welcome' => 'users#welcome'
   post '/logout' => 'users#destroy'
