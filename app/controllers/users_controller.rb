357267d7eb057f861bcb6cd1c4b4a341e52f7dbc
Emily Jirles <emily.jirles@gmail.com>
2018-06-22 18:47:30 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
   def name_present?
