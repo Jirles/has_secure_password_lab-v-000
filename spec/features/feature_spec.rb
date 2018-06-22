dbbeaafa537366f7caeefafeb2f6968f0015dc0c
Emily Jirles <emily.jirles@gmail.com>
2018-06-22 18:52:13 +0000

    automatically backed up by learn

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,3 +1,18 @@
 require 'rails_helper'
 
-describe ""
+describe "Secure Password Lab App" do 
+  context "user features" do 
+    it 'takes users to the login page as the root path' do 
+      visit root_path
+
+      expect(page).to have_content("Login")
+    end 
+
+    it 'login page/root path also has a link to signup for new users' do 
+      visit root_path 
+
+      expect(page).to have_link("Signup")
+    end 
+  end 
+  
+end 
